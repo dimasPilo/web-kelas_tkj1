94040215ba456a9b3d8ea57c1f22f0558a5336f3
Smzx <cici.mk.sk.12@gmail.com>
2024-11-26 12:44:47 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//menu
+
+
+
+$('#menu-btn').click(function(){
+    $('nav .navigation ul').addClass('active')
+  });
+
+  $('#menu-close').click(function(){
+    $('nav .navigation ul').removeClass('active')
+  });
+
+
+
+  //leaunge
